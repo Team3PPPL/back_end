8be832f81c45a9de8e02a21f29f2684d5baf1d22
ridho237 <ahmadridwan227@gmail.com>
2024-11-12 10:40:34 +0700

    Initialization Last

--- services/dbconnect.js
+++ services/dbconnect.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
-const serviceAccount = JSON.parse(serviceAccountJson);
+const serviceAccount = {
+	type: 'service_account',
+	project_id: process.env.FIREBASE_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_AUTH_URI,
+	token_uri: process.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+	client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+	universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 initializeApp({
 	credential: cert(serviceAccount),
