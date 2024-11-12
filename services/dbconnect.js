be8e570b93b7c1eac6b4a546c19e563ab39ce7bd
ridho237 <ahmadridwan227@gmail.com>
2024-11-12 09:26:43 +0700

    ENVIRONMENT 2

--- services/dbconnect.js
+++ services/dbconnect.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PPPL3_BACKEND);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_PPPL3_BACKEND);
 
 initializeApp({
 	credential: cert(serviceAccount),
