9ba7d9eb3868640f1defd37e720832a9f866cac0
ridho237 <ahmadridwan227@gmail.com>
2024-11-13 12:42:37 +0700

    Hosting to Vercel Final

--- index.js
+++ index.js
@@ -4,28 +4,20 @@ const app = express();
 const cashin = require('./routes/pemasukan');
 const cashout = require('./routes/pengeluaran');
 
-const startoServer = async () => {
-	const port = 3000;
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
-	app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-	try {
-		app.get('/', (req, res) => {
-			res.sendFile(path.join(__dirname, 'public', 'index.html'));
-		});
-
-		app.use('/', cashin);
-		app.use('/', cashout);
-
-		app.listen(port, () => {
-			console.log(`http://localhost:${port}`);
-		});
-	} catch (error) {
-		console.error('Stack trace:', error.stack);
-	}
-};
-
-startoServer().catch((err) => {
-	console.error('Error yang tidak terduga saat Server mulai: ', err.message);
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.use('/', cashin);
+app.use('/', cashout);
+
+app.use((err, req, res, next) => {
+	console.error('Stack trace:', err.stack);
+	res.status(500).send('Something broke!');
+});
+
+module.exports = app;
